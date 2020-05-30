e20d4f5cee700c8dc9234732e8a8db57ca11611b
dimitryku <diman.kuzmin347@gmail.com>
2020-05-30 23:17:54 +0300

    verticle added

---
+++ OstovDemo/Verticle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class verticle
+{
+    private string name;
+
+    public verticle()
+	{
+
+    }
+
+    public string GetName()
+    {
+        return this.name;
+    }
+
+    public void SetName(string _name)
+    {
+        this.name = _name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return base.Equals(obj);
+    }
+}
