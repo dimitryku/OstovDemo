6ca9de43618c335937dabeaa6a8eb02f0759eb21
Алексей Лысов <lexazombike72@gmail.com>
2020-06-02 18:50:56 +0300

    add blanks for cruskalform

--- OstovDemo/CruskalFormcs.cs
+++ OstovDemo/CruskalFormcs.cs
@@ -16,5 +16,102 @@ namespace OstovDemo
         {
             InitializeComponent();
         }
+
+        enum DemoState
+        {
+            NotStarted,
+            Going,
+            Paused
+        }
+
+        enum DemoMode
+        {
+            Fast,
+            Slow,
+            Manual
+        }
+
+        private DemoState curState = DemoState.NotStarted;
+        private DemoMode curMode;
+
+        private void CruskalFormcs_Load(object sender, EventArgs e)
+        {
+            // TODO shit
+            curMode = DemoMode.Slow;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // TODO close
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // TODO reset
+        }
+
+        private void next_btn_Click(object sender, EventArgs e)
+        {
+            if (curMode != DemoMode.Manual)
+            {
+                timer1.Stop();
+                timer1_Tick(null, null);
+                timer1.Start();
+            }
+            else
+            {
+                timer1_Tick(null, null);
+            }
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            switch (curState)
+            {
+                case DemoState.NotStarted:
+                    // TODO start
+                    break;
+                case DemoState.Going:
+                    // TODO pause
+                    break;
+                case DemoState.Paused:
+                    // TODO start
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        private void rb_fast_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb_fast.Checked)
+            {
+                curMode = DemoMode.Fast;
+                // TODO
+            }
+        }
+
+        private void rb_slow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb_slow.Checked)
+            {
+                curMode = DemoMode.Slow;
+                // TODO
+            }
+        }
+
+        private void rb_manual_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb_manual.Checked)
+            {
+                curMode = DemoMode.Manual;
+                // TODO
+            }
+        }
     }
 }
