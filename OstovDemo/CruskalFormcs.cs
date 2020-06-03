ef537aca4c34f897672ba625eb856f479296b645
dimitryku <diman.kuzmin347@gmail.com>
2020-06-03 13:48:47 +0300

    2 lines commit

--- OstovDemo/CruskalFormcs.cs
+++ OstovDemo/CruskalFormcs.cs
@@ -218,8 +218,9 @@ namespace OstovDemo
                     start_btn.Enabled = false;
                     if (currentEdge < Edges.Count() - 1)
                         log_tb.Text += "Остальные ребра не подходят" + Environment.NewLine;
+                    else log_tb.Text += "Конец списка рёбер" + Environment.NewLine;
 
-                    if(curMode != DemoMode.NoAnime)
+                    if (curMode != DemoMode.NoAnime)
                         MessageBox.Show("Метод завершил свою работу, все вершины присоединены.", "Готово!",
                         MessageBoxButtons.OK);
                     drawing_panel.Invalidate();
